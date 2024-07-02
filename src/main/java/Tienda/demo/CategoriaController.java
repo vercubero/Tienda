0ebb915ffce0aa96e2ec473bfea62f42962fc8af
DELL <DELL@DESKTOP-UTOHSLK>
2024-07-02 15:45:02 -0600

    Finalizacion codigo sem 5

--- src/main/java/Tienda/demo/CategoriaController.java
+++ src/main/java/Tienda/demo/CategoriaController.java
@@ -4,21 +4,29 @@
  */
 package Tienda.demo;
 
+import Tienda.demo.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author DELL
- */
+
 @Controller
+@RequestMapping ("/categoria")
 public class CategoriaController {
     
-    @RequestMapping("/url")
-    public String page(Model model) {
-        model.addAttribute("attribute", "value");
-        return "view.name";
+    @Autowired
+    CategoriaService categoriaservice;
+    
+    
+//    Posible error
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        List<Categoria> lista= categoriaService.getCategorias(false);
+        model.addAttribute("categorias", lista.size());
+        
+        return "/categoria/listado";
     }
     
 }
